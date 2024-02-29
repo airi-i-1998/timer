8e463a5295cbaa74a86dddab9f76f4f5393b10a3
anko.i1026@gmail.com <airi@inoueairinoMacBook-Pro.local>
2024-02-29 20:58:06 +0900

    add style

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed, onMounted } from 'vue';
+import { ref, computed, onMounted } from "vue";
 
 //現在の日時を処理
 const date = ref(new Date());
@@ -10,8 +10,8 @@ const hours = computed(() => dateTimePadding(date.value.getHours()));
 const minutes = computed(() => dateTimePadding(date.value.getMinutes()));
 const seconds = computed(() => dateTimePadding(date.value.getSeconds()));
 
-  // /日時を2桁に変換
-  function dateTimePadding(num){
+// /日時を2桁に変換
+function dateTimePadding(num) {
   // numが一桁の場合にも必ず2桁の文字列を作り出すための手法
   return ("0" + num).slice(-2);
 }
@@ -28,22 +28,60 @@ onMounted(() => {
   // 1秒ごとにsetDate()を実行し、computed（）の関数が連動して実行されることで時間が表示される
   // setInterval：一定の間隔で指定された関数を繰り返し実行するためのJavaScriptのメソッド
   setInterval(() => {
-    date.value = new Date();}, 1000);
+    date.value = new Date();
+  }, 1000);
 });
 </script>
 
 <template>
-  <div>
-    <div>
+  <div class="container">
+    <div class="date">
       <p>{{ year }}/{{ month }}/{{ day }}</p>
     </div>
     <div>
-      <p>
-        {{ hours }}:{{ minutes }}
-        <span>
-          :{{ seconds }}
-        </span>
+      <p class="time">
+        {{ hours }}：{{ minutes }}
+        <span class="seconds">：{{ seconds }}</span>
       </p>
     </div>
   </div>
 </template>
+
+<style scoped>
+.container {
+  width: 60%;
+  height: 100%;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+  padding: 40px;
+  margin: 25% 40px;
+  border-radius: 10px;
+}
+
+p {
+  margin: 0px;
+}
+
+.date,
+.time {
+  font-weight: 700;
+  color: lawngreen;
+}
+
+.date {
+  font-size: 20px;
+  justify-content: right;
+  padding-left: 40%;
+}
+
+.time {
+  font-size: 80px;
+}
+
+.seconds {
+  font-size: 40px;
+}
+</style>
